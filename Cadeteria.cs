25655939758002e77b07e996bcb5550da3573731
iroderick00 <iroderick@outlook.es>
2022-09-23 18:43:59 -0300

    gbfgbgb

--- Cadeteria.cs
+++ Cadeteria.cs
@@ -2,7 +2,12 @@ public class Cadeteria{
     string Nombre;
     long int Telefono;
     List<Cadete> Cadetes;
+    public
     Cadeteria(string nombre,long int telefono){
+        this->Nombre=nombre;
+        this->Telefono=telefonoM;
+    }
+    void AgregarCadete(Cadete){
         
     }
 
